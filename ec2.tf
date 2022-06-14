1d361cc0bdd1e84eec61249c386a0efa6749bf6f
jiggyjo <jigyasajoshi1000@gmail.com>
2022-06-14 11:51:21 +0000

    new variable files

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,9 @@
 provider "aws" {
-  access_key = "AKIA5RM2ZBEJSK3XPFED"
-  secret_key = "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"
-  region     = "ap-south-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "default"
+}
+resource "aws_instance" "my-instance126" {
+  ami           =  "ami-0d2986f2e8c0f7d01"
+instance_type  = var.ec2_instance_type
 }
 
-resource "aws_instance" "my-instance126" {
-  ami           = "ami-0a3277ffce9146b74"
-  instance_type = "t2.micro"
-}
--- vpc.tf
+++ vpc.tf
@@ -2,17 +2,14 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "my-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = var.vpc_cidr_block
 
   azs             = ["ap-south-1a"]
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
+  private_subnets = slice(var.private_subnet_cidr_blocks, 0, var.private_subnet_count)
+  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, var.public_subnet_count)
 
   enable_nat_gateway = true
-  enable_vpn_gateway = true
+  enable_vpn_gateway = var.enable_vpn_gateway
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+  tags = var.resource_tags
 }
